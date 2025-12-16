097f00cee71ae9edec1dc49ccdab5635767d7090
ifbemarhal <ifbemarhal@gmail.com>
2025-12-16 14:44:31 +0900

    핸드폰 ui 에셋

--- Assets/3.Script/Dijkstraza/ScrollConfigWindow.cs
+++ Assets/3.Script/Dijkstraza/ScrollConfigWindow.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 public class testScroll : MonoBehaviour
@@ -33,7 +34,7 @@ public class testScroll : MonoBehaviour
     {
         //dijkstraza 테스트용
         //StartCoroutine(UpScroll());
-        //StartCoroutine(DownScroll());
+        StartCoroutine(DownScroll());
     }
 
     public void OnUpperScroll()
@@ -57,13 +58,19 @@ public class testScroll : MonoBehaviour
         //}
 
         //달성 시간을 정하여 그 시간안에 달성하게 하는 방식
+        float startPosY = _gameObject.transform.localPosition.y;
         float _elapseTime = 0;
+        Image image = _gameObject.GetComponent<Image>();
+        Color tempColor = image.color;
         while (_elapseTime < _duration)      //달성 시간과 비슷한 시간이 되면 while문을 벗어남
         {
             _elapseTime += Time.deltaTime;          //누적시간만들기
             float per = _elapseTime / _duration;    //누적시간이 지속시간에 비하여 몇%인지 확인하여 넣기
 
-            _gameObject.transform.localPosition = new Vector3(0, Mathf.Lerp(_gameObject.transform.localPosition.y, _targetPosition, _elapseTime), 0);
+            _gameObject.transform.localPosition = new Vector3(0, Mathf.Lerp(startPosY, _targetPosition, per), 0);
+            tempColor.a = Mathf.Lerp(0.3f, 1f, per);
+            image.color = tempColor;
+            
             yield return null;
         }
 
@@ -73,6 +80,8 @@ public class testScroll : MonoBehaviour
         // 마찬가지로 진행도가 0.4%씩 차는데 마지막이 99.7%이면 합했을 때 100.1%이므로 while문 내부를 실행하지 않고 밖으로 나가버림
         // 그러나 우리의 목표는 100%에 해당하는 값임, 이 차이를 보정하기 위해 최종적으로 대상의 position에 목표 position값을 대입해버림
         _gameObject.transform.localPosition = new Vector3(0, _targetPosition, 0);
+        tempColor.a = 1;
+        image.color = tempColor;
     }
 
     IEnumerator UpScroll()
