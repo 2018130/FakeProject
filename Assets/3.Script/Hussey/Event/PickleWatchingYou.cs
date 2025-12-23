d3c00d0b24f2df864dc2f05533f1a9a20752794d
1991jkh <demonicjkh@gmail.com>
2025-12-23 17:54:27 +0900

    not today

--- Assets/3.Script/Hussey/Event/PickleWatchingYou.cs
+++ Assets/3.Script/Hussey/Event/PickleWatchingYou.cs
@@ -12,24 +12,28 @@ public class PickleWatchingYou : MonoBehaviour
     [SerializeField] private float walkSpeed = 3f;
     [SerializeField] private float lookWeight = 1f;
 
+    [SerializeField] Transform headTransform;
+
     private Animator ani;
+
+
 
     private void Awake()
     {
         pickle = FindAnyObjectByType<Pickle>();
         sceneContext = FindAnyObjectByType<SceneContext>();
-        TryGetComponent(out ani);
+        ani = pickle.GetComponentInChildren<Animator>();
     }
 
     /// <summary>
     /// pickle must be humanoid type
     /// using avatar mask. separate head and body -> base => body
     /// </summary>
-    //public void SetLookat()
-    //{
-    //    ani.SetLookAtWeight(lookWeight);
-    //    ani.SetLookAtPosition(sceneContext.Player.transform.position);
-    //}
+    public void SetLookat()
+    {
+        ani.SetLookAtWeight(lookWeight);
+        ani.SetLookAtPosition(sceneContext.Player.transform.position);
+    }
 
     public void WatchingYou()
     {
@@ -44,5 +48,18 @@ public class PickleWatchingYou : MonoBehaviour
 
         //SoundManager.Instance.PlaySFX(ESFX.SFX_PickleStep);
         //SoundManager.Instance.PlaySFX(ESFX.SFX_PickleBreath1);
+    }
+
+    public void OnAnimatorIK(int layerIndex)
+    {
+        Debug.Log("1111");
+        //ani.SetLookAtWeight(lookWeight);
+        //ani.SetLookAtPosition(sceneContext.Player.transform.position);
+
+        Vector3 dir = (sceneContext.Player.transform.position - headTransform.position); // world space target direction
+        dir = headTransform.InverseTransformDirection(dir); // to head local space target direction
+        Quaternion headRot = Quaternion.LookRotation(dir); // Get local quaternion
+
+        ani.SetBoneLocalRotation(HumanBodyBones.Head, headRot); // apply to bone
     }
 }
