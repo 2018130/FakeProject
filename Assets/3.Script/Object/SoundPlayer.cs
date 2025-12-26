da3c08dee8fa0ce892c35cafddef98c4c885c928
agucangman <hha_1@naver.com>
2025-12-26 14:51:29 +0900

    오디오 추가

--- Assets/3.Script/Object/SoundPlayer.cs
+++ Assets/3.Script/Object/SoundPlayer.cs
@@ -7,12 +7,17 @@ public class SoundPlayer : MonoBehaviour
 {
     [SerializeField]
     private AudioClip audioClip;
+    [SerializeField]
+    private ESFX sfxType;
 
     public void PlayeAudio()
     {
         SoundManager.Instance.PlaySFX(audioClip);
     }
-
+    public void PlayeAudioByType()
+    {
+        SoundManager.Instance.PlaySFX(sfxType);
+    }
     public void StopAudio()
     {
         SoundManager.Instance.StopSFX();
